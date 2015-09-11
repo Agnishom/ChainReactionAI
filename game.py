5856cb975fb78d76a25e918edb0d7209a9a0483b
Agnishom <Cagnishom@gmail.com>
2015-09-11 20:44:57 +0530

    The game works

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import minimax, structure
+import thread
 
 m, n = 9, 6
 
@@ -11,7 +12,9 @@ font = pygame.font.Font('Font.ttf', 48)
 def drawBoard(board=structure.Board()):
 	surface.fill((0,0,0))
 	for pos in [(x,y) for x in xrange(board.m) for y in xrange(board.n)]:
-		if structure.sgn(board[pos]) == 0:
+		if abs(board[pos]) >= board.critical_mass(pos):
+			color = (255,255,0)
+		elif structure.sgn(board[pos]) == 0:
 			color = (255,255,255)
 		elif structure.sgn(board[pos]) == 1:
 			color = (255,0,0)
@@ -22,6 +25,26 @@ def drawBoard(board=structure.Board()):
 		surface.blit(text, textpos)
 	pygame.display.update()
 
+def slowMove(board, pos):
+	board = structure.copy.deepcopy(board)
+	assert board.new_move == structure.sgn(board[pos]) or 0 == structure.sgn(board[pos])
+	board[pos] = board[pos] + board.new_move
+	while True:
+		drawBoard(board)
+		pygame.time.wait(250)
+		unstable = []
+		for pos in [(x,y) for x in xrange(board.m) for y in xrange(board.n)]:
+			if abs(board[pos]) >= board.critical_mass(pos):
+				unstable.append(pos)
+		#raw_input()
+		if not unstable:
+			break
+		for pos in unstable:
+			board[pos] -= board.new_move*board.critical_mass(pos)
+			for i in board.neighbors(pos):
+				board[i] = structure.sgn(board.new_move)*(abs(board[i])+1)
+	drawBoard(board)
+
 def main():
 	board = structure.Board()
 	drawBoard(board)
@@ -31,8 +54,12 @@ def main():
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				x,y = pygame.mouse.get_pos()
 				x,y = x/50,y/50
-				try:
-					board = structure.move(board,(y,x))
-					drawBoard(board)
-				except:
-					pass
+				thread.start_new_thread(slowMove, (board,(y,x)))
+				board = structure.move(board,(y,x))
+				new_move = minimax.minimax(board)[0]
+				print "am here"
+				thread.start_new_thread(slowMove, (board, new_move))
+				board = structure.move(board, new_move)
+
+if __name__ == "__main__":
+	main()
